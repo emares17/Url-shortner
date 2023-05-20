7ea9ff02d0df03b4b9a231b0e0e0077d2481e15c
Esteban Mares <estebanmares17@gmail.com>
2023-05-20 15:07:11 -0700

    continued working on styles

--- server/public/main.js
+++ server/public/main.js
@@ -1,9 +1,18 @@
-var hamburgerMenu = document.querySelector(".hamburger-menu");
-var menuList = document.querySelector(".menu-list");
-var mobileMenu = document.querySelector(".mobile-menu");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 // Toggle the active class on the hamburger menu and mobile menu
-hamburgerMenu.addEventListener("click", function() {
+hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
   mobileMenu.classList.toggle("active");
 });
+
+const closeNavMenuOutside = (event) => {
+  if (!mobileMenu.contains(event.target) && !hamburgerMenu.contains(event.target)) {
+    hamburgerMenu.classList.remove('active');
+    mobileMenu.classList.remove('active');
+  }
+}
+
+document.addEventListener('click', closeNavMenuOutside);
+
