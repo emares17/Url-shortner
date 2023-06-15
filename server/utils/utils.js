30d2480b93a841f88d4a76e50d69a130b4e3d8eb
Esteban Mares <estebanmares17@gmail.com>
2023-06-14 21:26:40 -0700

    added pagination sort to utils

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -3,7 +3,7 @@ const Url = require('../models/Url');
 const getShortUrls = async (page, limit) => {
   const skip = page * limit;
   const totalUrls = await Url.countDocuments();
-  const shortUrl = await Url.find().skip(skip).limit(limit).exec();
+  const shortUrl = await Url.find().sort({ _id: -1 }).skip(skip).limit(limit).exec();
 
   return {
     shortUrl: shortUrl,
